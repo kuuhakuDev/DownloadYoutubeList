58624ad1c90056f0f69c5f05f39c9f19c825cf2c
Kuuhaku <KuuhakuDeveloper@outlook.com>
2018-07-31 20:38:49 -0600

    Agregando los argumentos

--- DownloadYoutubeList.py
+++ DownloadYoutubeList.py
@@ -7,3 +7,8 @@
 import urllib2
 import argparse
 
+parse = argparse.ArgumentParse()
+parse.add_argument("-u", "--url", help="Introduce la lista de reproduccion a descargar")
+parse.add_argument("-w", "--write", help="Archivo de salida")
+parse.add_argument("-p", "--program", help="El programa que descargara la lista de reproduccion")
+argv = parse.parse_args()
