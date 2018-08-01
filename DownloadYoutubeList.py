9ec7d780a534024f57c2d98dedddf933b4d6d6c4
Kuuhaku <KuuhakuDeveloper@outlook.com>
2018-07-31 22:22:10 -0600

    Agregada la funcion para escribir las urls en un fichero

--- DownloadYoutubeList.py
+++ DownloadYoutubeList.py
@@ -8,6 +8,7 @@ import urllib2
 import argparse
 
 href = "pl-video-title-link yt-uix-tile-link yt-uix-sessionlink  spf-link "
+file = "youtube.txt"
 
 parse = argparse.ArgumentParser()
 parse.add_argument("-u", "--url", help="Introduce la lista de reproduccion a descargar")
@@ -15,6 +16,8 @@ parse.add_argument("-w", "--write", help="Archivo de salida")
 parse.add_argument("-p", "--program", help="El programa que descargara la lista de reproduccion")
 argv = parse.parse_args()
 
+
+#Extraer los links de los videos
 if argv.url:
 	lineas = []
 	for linea in urllib2.urlopen(argv.url):
@@ -26,5 +29,16 @@ if argv.url:
 		index 		= linea.find("href=\"")
 		indexInicio = linea.find("\"", index, len(linea)) + 1
 		indexFin 	= linea.find("&", indexInicio, len(linea)) 
-		links.append(linea[indexInicio:indexFin])
+		links.append("https://youtube.com" + linea[indexInicio:indexFin])
 
+#Escribri los links en pantalla
+if argv.write:
+	file = open(argv.write, "w")
+	for linea in links:
+		file.write(linea + "\n")
+	file.close()
+else:
+	file = open(file, "w")
+	for linea in links:
+		file.write(linea + "\n")
+	file.close()
