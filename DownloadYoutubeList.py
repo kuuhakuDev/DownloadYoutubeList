a03088a3d45c4eff2625f1715299917b065212e2
Kuuhaku <KuuhakuDeveloper@outlook.com>
2018-07-31 21:35:04 -0600

    Agregada la descarga del codigo fuente y seleccionar las lineas necesarias

--- DownloadYoutubeList.py
+++ DownloadYoutubeList.py
@@ -7,8 +7,17 @@
 import urllib2
 import argparse
 
-parse = argparse.ArgumentParse()
+href = "pl-video-title-link yt-uix-tile-link yt-uix-sessionlink  spf-link "
+
+parse = argparse.ArgumentParser()
 parse.add_argument("-u", "--url", help="Introduce la lista de reproduccion a descargar")
 parse.add_argument("-w", "--write", help="Archivo de salida")
 parse.add_argument("-p", "--program", help="El programa que descargara la lista de reproduccion")
 argv = parse.parse_args()
+
+if argv.url:
+	for linea in urllib2.urlopen(argv.url):
+		if href in linea:
+			print linea
+
+	
